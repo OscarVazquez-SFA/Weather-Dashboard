459858e5f1865dcacb7c7a29081518a9367809f3
OscarVazquez-SFA <vazquezo1@jacks.sfasu.edu>
2025-07-16 23:22:22 -0500

    added card component from Daisy UI into the CurrentDay react component

--- Weather-Dashboard/src/CurrentDay.jsx
+++ Weather-Dashboard/src/CurrentDay.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-export default function CurrentDay(){
-    return(
-        <div>
-            <h1 className="text-2xl font-bold">Current Day</h1>
-            <p className="text-lg">Weather details will be displayed here.</p>
+export default function CurrentDay() {
+    return (
+        <div class="card bg-base-100 image-full w-96 shadow-sm">
+            <figure>
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Current Day of the Week here!</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     )
 }
