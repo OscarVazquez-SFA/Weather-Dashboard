4b158aa332417c4a5043213d68d21ecaab8927ba
OscarVazquez-SFA <vazquezo1@jacks.sfasu.edu>
2025-07-16 22:51:12 -0500

    fixed error #5. added component (CurrentDay) to the App.jsx file

--- Weather-Dashboard/src/App.jsx
+++ Weather-Dashboard/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CurrentDay from './CurrentDay.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <CurrentDay />
       <label className="swap swap-rotate">
         {/* this hidden checkbox controls the state */}
         <input type="checkbox" className="theme-controller" value="light" />
---
+++ Weather-Dashboard/src/CurrentDay.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CurrentDay(){
+    return(
+        <div>
+            <h1 className="text-2xl font-bold">Current Day</h1>
+            <p className="text-lg">Weather details will be displayed here.</p>
+        </div>
+    )
+}
