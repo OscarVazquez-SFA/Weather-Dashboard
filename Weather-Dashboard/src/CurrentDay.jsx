6f9432fb04d4eb526b92be31989d1e244c9c6d1b
OscarVazquez-SFA <vazquezo1@jacks.sfasu.edu>
2025-07-16 23:40:55 -0500

    Added day of the week to the card

--- Weather-Dashboard/src/CurrentDay.jsx
+++ Weather-Dashboard/src/CurrentDay.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 export default function CurrentDay() {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const date = new Date();
+    const day = weekday[date.getDay()];
     return (
-        <div class="card bg-base-100 image-full w-96 shadow-sm">
+       
+
+        <div className="card bg-base-100 image-full w-96 shadow-sm">
             <figure>
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
                     alt="Shoes" />
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">Current Day of the Week here!</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
-                </div>
+            <div className="card-body">
+                <h2 className="card-title">{day}</h2>
+                <p>temperature, wind speed, humidity percentange along with icon of the current weather (sunny, cloudy, rainy, etc)</p>
             </div>
         </div>
     )
