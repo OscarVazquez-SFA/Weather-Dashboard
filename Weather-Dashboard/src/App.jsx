0b242d086ca4f830db2beb0292275acef87408a7
OscarVazquez-SFA <vazquezo1@jacks.sfasu.edu>
2025-07-16 22:36:55 -0500

    Set up a new component that will represent the current day to the user.

--- Weather-Dashboard/src/App.jsx
+++ Weather-Dashboard/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <label className="swap swap-rotate">
+    <label className="swap swap-rotate">
   {/* this hidden checkbox controls the state */}
   <input type="checkbox" className="theme-controller" value="light" />
 
