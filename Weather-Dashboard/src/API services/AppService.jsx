579b1afa38dac8db7dabaaf0eae6651d44cad59c
OscarVazquez-SFA <vazquezo1@jacks.sfasu.edu>
2025-08-25 10:28:22 -0500

    Changed log message to debug error msg more.

--- Weather-Dashboard/src/API services/AppService.jsx
+++ Weather-Dashboard/src/API services/AppService.jsx
@@ -8,7 +8,7 @@ more ppl are starting to shift to useing async/await syntax rather than promises
 export default async function AppServices(city, api_key) {
     const result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`);
     if (!result.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error("AppService Comp: Network response was not ok");
     }
     const data = await result.json();
     return data;
