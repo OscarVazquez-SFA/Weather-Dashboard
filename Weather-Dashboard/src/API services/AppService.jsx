e6de74ee6880e074cc0557cfa9d38b92c211df46
OscarVazquez-SFA <vazquezo1@jacks.sfasu.edu>
2025-11-16 14:28:59 -0600

    added s

--- Weather-Dashboard/src/API services/AppService.jsx
+++ Weather-Dashboard/src/API services/AppService.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 more ppl are starting to shift to useing async/await syntax rather than promises, so we will use that here
 */
 export default async function AppServices(city, api_key) {
-    const result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`);
+    const result = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`);
     if (!result.ok) {
         throw new Error("AppService Comp: Network response was not ok");
     }
